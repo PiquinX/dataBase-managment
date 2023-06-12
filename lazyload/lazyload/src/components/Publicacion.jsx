05653ae143703fe5571554a5524167effd5d899b
Santy <sanpiquin@gmail.com>
2023-06-11 22:48:59 -0300

    falta que funcione el contador

--- lazyload/lazyload/src/components/Publicacion.jsx
+++ lazyload/lazyload/src/components/Publicacion.jsx
@@ -1,4 +1,4 @@
-function Post ({ userName, userPhoto, photo, description }) {
+export function Post ({ userName, userPhoto, photo, description }) {
   return (
     <section className='post'>
       <div className='post-header'>
@@ -15,5 +15,3 @@ function Post ({ userName, userPhoto, photo, description }) {
     </section>
   )
 }
-
-export default Post
---
+++ lazyload/lazyload/src/constants.js
@@ -0,0 +1,80 @@
+export const users = [{
+  photo: 'https://marketplace.canva.com/EAFHm4JWsu8/1/0/1600w/canva-pink-landscape-desktop-wallpaper-HGxdJA_xIx0.jpg',
+  userPhoto: 'https://img.redbull.com/images/c_crop,x_1676,y_0,h_1310,w_1048/c_fill,w_860,h_1075/q_auto:low,f_auto/redbullcom/2023/2/22/qpctvo5nspffj1vgy3a1/red-bull-click-cabecera',
+  userName: 'Di gref',
+  desc: 'Un gran wallpaper'
+}, {
+  photo: 'https://img.freepik.com/free-photo/blossom-floral-bouquet-decoration-colorful-beautiful-flowers-background-garden-flowers-plant-pattern-wallpapers-greeting-cards-postcards-design-wedding-invites_90220-1103.jpg',
+  userPhoto: 'https://pbs.twimg.com/media/E67Rji2WUAg9ysx?format=jpg&name=4096x4096',
+  userName: 'Ramiro',
+  desc: 'Aaaaaaaaaaaaaa'
+}, {
+  photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc0wNrU_mRDrNPROBMUx88oluxTNpTEx95GThBwkj7pQ&s',
+  userPhoto: 'https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQfh3tb4b_7f4rkGKqjsC6mH_ySoIQ3_6Sa_qKRTnU7jDK4xF7nv82G-6EK2HtUSeGaCUnD7uSncxw3Gs6in8tYVjtb9X0rI_46pnrFz2Uu0mQZRfz5XrLw0ROtTNYUJh3ghm79452H6QiAA5mIm_ArDJ.jpg?r=eb4',
+  userName: 'Tomas',
+  desc: 'Esta re locoooo'
+},
+{
+  photo: 'https://marketplace.canva.com/EAFHm4JWsu8/1/0/1600w/canva-pink-landscape-desktop-wallpaper-HGxdJA_xIx0.jpg',
+  userPhoto: 'https://img.redbull.com/images/c_crop,x_1676,y_0,h_1310,w_1048/c_fill,w_860,h_1075/q_auto:low,f_auto/redbullcom/2023/2/22/qpctvo5nspffj1vgy3a1/red-bull-click-cabecera',
+  userName: 'Di gref',
+  desc: 'Un gran wallpaper'
+}, {
+  photo: 'https://img.freepik.com/free-photo/blossom-floral-bouquet-decoration-colorful-beautiful-flowers-background-garden-flowers-plant-pattern-wallpapers-greeting-cards-postcards-design-wedding-invites_90220-1103.jpg',
+  userPhoto: 'https://pbs.twimg.com/media/E67Rji2WUAg9ysx?format=jpg&name=4096x4096',
+  userName: 'Ramiro',
+  desc: 'Aaaaaaaaaaaaaa'
+}, {
+  photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc0wNrU_mRDrNPROBMUx88oluxTNpTEx95GThBwkj7pQ&s',
+  userPhoto: 'https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQfh3tb4b_7f4rkGKqjsC6mH_ySoIQ3_6Sa_qKRTnU7jDK4xF7nv82G-6EK2HtUSeGaCUnD7uSncxw3Gs6in8tYVjtb9X0rI_46pnrFz2Uu0mQZRfz5XrLw0ROtTNYUJh3ghm79452H6QiAA5mIm_ArDJ.jpg?r=eb4',
+  userName: 'Tomas',
+  desc: 'Esta re locoooo'
+},
+{
+  photo: 'https://marketplace.canva.com/EAFHm4JWsu8/1/0/1600w/canva-pink-landscape-desktop-wallpaper-HGxdJA_xIx0.jpg',
+  userPhoto: 'https://img.redbull.com/images/c_crop,x_1676,y_0,h_1310,w_1048/c_fill,w_860,h_1075/q_auto:low,f_auto/redbullcom/2023/2/22/qpctvo5nspffj1vgy3a1/red-bull-click-cabecera',
+  userName: 'Di gref',
+  desc: 'Un gran wallpaper'
+}, {
+  photo: 'https://img.freepik.com/free-photo/blossom-floral-bouquet-decoration-colorful-beautiful-flowers-background-garden-flowers-plant-pattern-wallpapers-greeting-cards-postcards-design-wedding-invites_90220-1103.jpg',
+  userPhoto: 'https://pbs.twimg.com/media/E67Rji2WUAg9ysx?format=jpg&name=4096x4096',
+  userName: 'Ramiro',
+  desc: 'Aaaaaaaaaaaaaa'
+}, {
+  photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc0wNrU_mRDrNPROBMUx88oluxTNpTEx95GThBwkj7pQ&s',
+  userPhoto: 'https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQfh3tb4b_7f4rkGKqjsC6mH_ySoIQ3_6Sa_qKRTnU7jDK4xF7nv82G-6EK2HtUSeGaCUnD7uSncxw3Gs6in8tYVjtb9X0rI_46pnrFz2Uu0mQZRfz5XrLw0ROtTNYUJh3ghm79452H6QiAA5mIm_ArDJ.jpg?r=eb4',
+  userName: 'Tomas',
+  desc: 'Esta re locoooo'
+},
+{
+  photo: 'https://marketplace.canva.com/EAFHm4JWsu8/1/0/1600w/canva-pink-landscape-desktop-wallpaper-HGxdJA_xIx0.jpg',
+  userPhoto: 'https://img.redbull.com/images/c_crop,x_1676,y_0,h_1310,w_1048/c_fill,w_860,h_1075/q_auto:low,f_auto/redbullcom/2023/2/22/qpctvo5nspffj1vgy3a1/red-bull-click-cabecera',
+  userName: 'Di gref',
+  desc: 'Un gran wallpaper'
+}, {
+  photo: 'https://img.freepik.com/free-photo/blossom-floral-bouquet-decoration-colorful-beautiful-flowers-background-garden-flowers-plant-pattern-wallpapers-greeting-cards-postcards-design-wedding-invites_90220-1103.jpg',
+  userPhoto: 'https://pbs.twimg.com/media/E67Rji2WUAg9ysx?format=jpg&name=4096x4096',
+  userName: 'Ramiro',
+  desc: 'Aaaaaaaaaaaaaa'
+}, {
+  photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc0wNrU_mRDrNPROBMUx88oluxTNpTEx95GThBwkj7pQ&s',
+  userPhoto: 'https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQfh3tb4b_7f4rkGKqjsC6mH_ySoIQ3_6Sa_qKRTnU7jDK4xF7nv82G-6EK2HtUSeGaCUnD7uSncxw3Gs6in8tYVjtb9X0rI_46pnrFz2Uu0mQZRfz5XrLw0ROtTNYUJh3ghm79452H6QiAA5mIm_ArDJ.jpg?r=eb4',
+  userName: 'Tomas',
+  desc: 'Esta re locoooo'
+},
+{
+  photo: 'https://marketplace.canva.com/EAFHm4JWsu8/1/0/1600w/canva-pink-landscape-desktop-wallpaper-HGxdJA_xIx0.jpg',
+  userPhoto: 'https://img.redbull.com/images/c_crop,x_1676,y_0,h_1310,w_1048/c_fill,w_860,h_1075/q_auto:low,f_auto/redbullcom/2023/2/22/qpctvo5nspffj1vgy3a1/red-bull-click-cabecera',
+  userName: 'Di gref',
+  desc: 'Un gran wallpaper'
+}, {
+  photo: 'https://img.freepik.com/free-photo/blossom-floral-bouquet-decoration-colorful-beautiful-flowers-background-garden-flowers-plant-pattern-wallpapers-greeting-cards-postcards-design-wedding-invites_90220-1103.jpg',
+  userPhoto: 'https://pbs.twimg.com/media/E67Rji2WUAg9ysx?format=jpg&name=4096x4096',
+  userName: 'Ramiro',
+  desc: 'Aaaaaaaaaaaaaa'
+}, {
+  photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc0wNrU_mRDrNPROBMUx88oluxTNpTEx95GThBwkj7pQ&s',
+  userPhoto: 'https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQfh3tb4b_7f4rkGKqjsC6mH_ySoIQ3_6Sa_qKRTnU7jDK4xF7nv82G-6EK2HtUSeGaCUnD7uSncxw3Gs6in8tYVjtb9X0rI_46pnrFz2Uu0mQZRfz5XrLw0ROtTNYUJh3ghm79452H6QiAA5mIm_ArDJ.jpg?r=eb4',
+  userName: 'Tomas',
+  desc: 'Esta re locoooo'
+}]
