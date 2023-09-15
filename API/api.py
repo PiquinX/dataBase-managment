59aead524ae342b22fdc91d287439e6f6cf58707
Your Name <you@example.com>
2023-09-15 17:25:55 -0300

    api update added

--- API/api.py
+++ API/api.py
@@ -40,8 +40,33 @@ def get_all_by_ID(id):
         return jsonify(data)
     
 @app.route('/update_data/<int:id>', methods=['PUT'])
-def update_data():
-    pass
+def update_data(id):
+    with sqlite3.connect('data_base.db') as conn:
+        values = request.get_json()
+        cursor = conn.cursor()
+        queries = [
+            f"""UPDATE usuarios SET ESTADO_DE_USUARIO = {values['estado']}, TIPO_DE_USUARIO = {values['tipo']} , DNI = {values['dni']}, FECHA_DE_NACIMIENTO = {values['nacimiento']}, APELLIDO = {values['apellido']}, NOMBRE = {values['nombre']}, 
+                                    MAIL = {values['mail']}, CUIL_CUIT = {values['cuil']}, TELEFONO_MOVIL = {values['movil']}, TELEFONO_FIJO = {values['fijo']}, REFERENTE = {values['referente']}, OCUPACION = {values['ocupcacion']}, DIA_DE_ALTA = {values['fechaDeAlta']},
+                                    FIRMA = {values['firma']} WHERE USUARIO_ID = {id}""",
+
+            f"""UPDATE observaciones SET OBSERVACIONES = {values['observacion']} WHERE USUARIO_ID = {id}""",
+
+            f"""UPDATE donaciones SET DONACION = {values['cantidad']}, FECHA_DE_DONACION = {values['fecha']}, ESTADO_DE_DONACION = {values['estado']}, TIPO_DE_DONACION = {values['tipo']}, FORMA_DE_PAGO = {values['metodoDePago']} WHERE USUARIO_ID={id}""",
+
+            f"""UPDATE direccion SET CALLE = {values['calle']}, NUM = {values['numero']}, PISO = {values['piso']}, DEPTO = {values['depto']}, LOCALIDAD = {values['localidad']}, CODIGO_POSTAL = {values['codigoPostal']}, PROVINCIA = {values['provincia']} WHERE USUARIO_ID = {id}""",
+
+            f"""UPDATE datos_financiero SET DBTO = {values['debito']}, VTO = {values['vto']}, COD_SEG = {values['codigoSeguridad']}, BANCO = {values['banco']}, SUCURSAL = {values['sucursal']}, TIPO_CTA = {values['tipoCTA']}, #_CUENTA = {values['']}, ESTADO = {values['estado']} WHERE USUARIO_ID = {id}"""
+
+        ]
+
+        try:
+            for query in queries:
+                cursor.execute(query)
+        except Exception as e:
+            return jsonify({"Messagge": f"error del tipo {e}"})
+        
+        return jsonify({"Messagge": "Datos actualizados correctamente"})
+       
     
 if __name__ == "__main__":
     app.run()
